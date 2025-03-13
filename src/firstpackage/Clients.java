89a573351e18302a374c7cd769c429954f8d62e1
Balkhi <balkhi.prof@gmail.com>
2025-03-13 14:16:06 +0000

    Première version du projet poo

---
+++ src/firstpackage/Clients.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package firstpackage;
+
+/**
+ *
+ * @author Balkhi
+ */
+public class Clients {
+    //variables d'instances
+    private String nom,prenom,adresse,tel,email;
+    //Constructeurs
+
+    public Clients(String nom, String prenom, String adresse, String tel) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.tel = tel;
+    }
+
+    public Clients(String nom, String prenom, String adresse, String tel, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.tel = tel;
+        this.email = email;
+    }
+    //Getters and setters
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
